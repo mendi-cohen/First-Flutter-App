029e89b875a639f514edb91650a67933f204513b
Your GitHub Username <your-email@example.com>
2024-02-04 16:23:28 +0200

    שיעורי בית 4

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:my_flutter_app/myDiv.dart';
-
-
 
 void main() {
   runApp(const MyApp());
@@ -11,51 +8,95 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
-
   Widget build(BuildContext context) {
-    return MaterialApp(home:HomeWidget());
+    return  MaterialApp(
+      home: Scaffold(
+        body:
+       Column(children:  [ Container(
+          width:400,
+          height: 200,
+          child: ListView(
+          scrollDirection: Axis.horizontal,
+          children:  [
+            MyBox(text: "number-1",), MyBox(text: "number-1",), MyBox(text: "number-1",),
+           MyBox(text: "number-1",), MyBox(text: "number-1",), MyBox(text: "number-1",),
+            MyBox(text: "number-1",), MyBox(text: "number-1",), MyBox(text: "number-1",), 
+             MyBox(text: "number-1",), MyBox(text: "number-1",), MyBox(text: "number-1",),
+             MyBox(text: "number-1",), MyBox(text: "number-1",), MyBox(text: "number-1",),
+             MyBox(text: "number-1",), MyBox(text: "number-1",), MyBox(text: "number-1",),],)) ,
+             
+         Container(
+          width:400,
+           height: 200,
+          child: ListView(
+           
+          scrollDirection: Axis.vertical,
+          children:  [
+            MyBox(text:"number - 2 " ,color: "green",), MyBox(text:"number - 2 " ,color: "green",), MyBox(text:"number - 2 " ,color: "green",),
+           MyBox(text:"number - 2 " ,color: "green",), MyBox(text:"number - 2 " ,color: "green",), MyBox(text:"number - 2 " ,color: "green",),
+            MyBox(text:"number - 2 " ,color: "green",), MyBox(text:"number - 2 " ,color: "green",), MyBox(text:"number - 2 " ,color: "green",), 
+             MyBox(text:"number - 2 " ,color: "green",), MyBox(text:"number - 2 " ,color: "green",), MyBox(text:"number - 2 " ,color: "green",),
+             MyBox(text:"number - 2 " ,color: "green",), MyBox(text:"number - 2 " ,color: "green",), MyBox(text:"number - 2 " ,color: "green",),
+             MyBox(text:"number - 2 " ,color: "green",), MyBox(text:"number - 2 " ,color: "green",), MyBox(text:"number - 2 " ,color: "green",),],
+        )),
+        
+           Container(
+          width:400,
+           height: 200,
+          child: GridView.count(
+            crossAxisCount: 1,
+          scrollDirection: Axis.horizontal,
+          children:  [
+            MyBox(text:"number - 3 " ,color: "blue",), MyBox(text:"number - 3 " ,color: "blue",), MyBox(text:"number - 3 " ,color: "blue",),
+           MyBox(text:"number - 3 " ,color: "blue",), MyBox(text:"number - 3 " ,color: "blue",), MyBox(text:"number - 3 " ,color: "blue",),
+            MyBox(text:"number - 3 " ,color: "blue",), MyBox(text:"number - 3 " ,color: "blue",), MyBox(text:"number - 3 " ,color: "blue",), 
+             MyBox(text:"number - 3 " ,color: "blue",), MyBox(text:"number - 3 " ,color: "blue",), MyBox(text:"number - 3 " ,color: "blue",),
+             MyBox(text:"number - 3 " ,color: "blue",), MyBox(text:"number - 3 " ,color: "blue",), MyBox(text:"number - 3 " ,color: "blue",),
+             MyBox(text:"number - 3 " ,color: "blue",), MyBox(text:"number - 3 " ,color: "blue",), MyBox(text:"number - 3 " ,color: "blue",),],
+        )), 
+        
+              Container(
+          width:400,
+           height: 200,
+          child: GridView.count(
+            crossAxisCount: 2,
+          scrollDirection: Axis.vertical,
+          children:  [
+            MyBox(text:"number - 4 " ,color: "red",), MyBox(text:"number - 4 " ,color: "red",), MyBox(text:"number - 4 " ,color: "red",),
+           MyBox(text:"number - 4 " ,color: "red",), MyBox(text:"number - 4 " ,color: "red",), MyBox(text:"number - 4 " ,color: "red",),
+            MyBox(text:"number - 4 " ,color: "red",), MyBox(text:"number - 4 " ,color: "red",), MyBox(text:"number - 4 " ,color: "red",), 
+             MyBox(text:"number - 4 " ,color: "red",), MyBox(text:"number - 4 " ,color: "red",), MyBox(text:"number - 4 " ,color: "red",),
+             MyBox(text:"number - 4 " ,color: "red",), MyBox(text:"number - 4 " ,color: "red",), MyBox(text:"number - 4 " ,color: "red",),
+             MyBox(text:"number - 4 " ,color: "red",), MyBox(text:"number - 4 " ,color: "red",), MyBox(text:"number - 4 " ,color: "red",),],
+        )), ], ))) ;     
   }
+
 }
 
-class HomeWidget extends StatelessWidget {
-  HomeWidget({super.key});
-
-  @override
-
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(bottom:AppBar(title: MyDiv(text:"appBar" ,color:"green"))),
-      body: Container(
-        height: 1000,
-        width: 600,
-        
-
-        child: Column
-        (children: [
-          Row(children: [
-          MyDiv(text: 'Hello'),
-         Expanded(child: MyDiv(text: "Hello" ,color:"red")),
-          MyDiv(text: "Hello"),
-          ],),
-
-          Row(children: [
-          MyDiv(text: 'Hello'),
-          Expanded(child: MyDiv(text: "Hello" ,color:"red")),
-          MyDiv(text: "Hello"),
-          ],),
-
-          Row(children: [
-          MyDiv(text: 'Hello'),
-          Expanded(child: MyDiv(text: "Hello" ,color:"red")),
-          MyDiv(text: "Hello"),
-          ],)
-         
-        ],
-        ),
-      ),
-      
-    );
-  }
 
 
+class MyBox extends StatelessWidget {
+  final text;
+  final String color;
+Map<String , Color>colorMap = {
+"red":Colors.red,
+"yellow":Colors.yellow,
+"green": Colors.green,
+"blue": Colors.blue,
+};
+MyBox({this.text = "hi", this.color = "yellow" ,super.key});
+
+@override
+Widget build(BuildContext context) {
+return Container(
+  height: 100,
+  width: 100,
+  color: Colors.grey,
+  child: Card(
+    color: colorMap[color],
+    child: Text(text)
+),
+  
+);
+}
 }
